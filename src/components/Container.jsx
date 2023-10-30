1bd60d3c820de3f11f117873c76723451979a0a3
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-10-30 18:55:15 -0300

    feat: creating container component

---
+++ src/components/Container.jsx
@@ -0,0 +1,25 @@
+import "../styles/Container.css";
+
+const Container = ({ flag, alt, title, population, region, capital }) => {
+  return (
+    <div className="country-box">
+      <div className="flag-box">
+        <img src={flag} alt={alt} width={250} height={200} />
+      </div>
+      <div className="country-data">
+        <h3>{title}</h3>
+        <p className="country-data-p">
+          Population: <span className="data">{population}</span>
+        </p>
+        <p className="country-data-p">
+          Region: <span className="data">{region}</span>
+        </p>
+        <p className="country-data-p">
+          Capital: <span className="data">{capital}</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Container;
