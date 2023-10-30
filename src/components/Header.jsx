2a4676cf84aa701b22c5801c569375afed349187
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-10-30 18:55:43 -0300

    feat: finishing header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
+import "../styles/Header.css";
+import DarkMode from "./DarkMode";
 const Header = () => {
   return (
-    <header>
-      <h2>Where in the world?</h2>
-      {/* Dark Mode toggle goes here */}
+    <header className="main-header">
+      <div className="header-wrapper">
+        <h2>Where in the world?</h2>
+        <DarkMode text={"Dark Mode"} />
+      </div>
     </header>
   );
 };
