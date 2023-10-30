241c599b6718b7b0bca0c51a1d969f24ea5d01cb
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-10-30 18:55:30 -0300

    feat: finishing darkmode component

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -1,7 +1,11 @@
-const DarkMode = () => {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon } from "@fortawesome/free-regular-svg-icons";
+import "../styles/DarkMode.css";
+
+const DarkMode = ({ text }) => {
   return (
-    <div>
-      {/* Icon toggler goes here */}
+    <div className="theme-box">
+      <FontAwesomeIcon icon={faMoon} size="xl" />
       <p>{text}</p>
     </div>
   );
