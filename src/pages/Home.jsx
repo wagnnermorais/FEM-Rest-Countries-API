34f30a7b2e2082c07d24502d894dce3842d035c0
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-10-30 18:56:26 -0300

    feat: fetching data in home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,37 @@
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Container from "../components/Container";
+import "../styles/Home.css";
+import countries from "../../db/data.json";
+import { useLoadMore } from "../hooks/useLoadMore";
+
 const Home = () => {
-  return <div>Home</div>;
+  const { visibleRows, handleLoadMore } = useLoadMore(2);
+  return (
+    <div className="">
+      <Header />
+      <div className="container">
+        <div className="home-wrapper">
+          {countries.slice(0, visibleRows * 4).map((country) => (
+            <div key={country.name} className="country-container">
+              <Container
+                flag={country.flags.png}
+                alt={`${country.name} Flag`}
+                title={country.name}
+                population={country.population}
+                region={country.region}
+                capital={country.capital}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="button-box">
+        <Button onClick={handleLoadMore} text={"Load more"} />
+        <Button onClick={""} text={"Load less"} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
