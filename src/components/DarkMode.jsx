8f8be901944360609465304a25b0e0935b49b311
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-11-07 17:48:38 -0300

    refac: adding new icons to theme switching

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -1,12 +1,13 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
-
 import "../styles/DarkMode.css";
 
 const DarkMode = ({ text, onClick, isDarkMode }) => {
   return (
     <div className="theme-box" onClick={onClick}>
-      <FontAwesomeIcon icon={isDarkMode ? faSun : faMoon} size="xl" />
+      {isDarkMode ? (
+        <ion-icon name="sunny-outline" className="theme-icon" />
+      ) : (
+        <ion-icon name="moon-outline" className="theme-icon" />
+      )}
       <p>{isDarkMode ? "Light Mode" : text}</p>
     </div>
   );
