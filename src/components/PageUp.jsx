2895a63b59c5fb03917dec22e4e0a2652e480045
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-11-07 17:48:54 -0300

    feat: creating page up component

---
+++ src/components/PageUp.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import "../styles/PageUp.css";
+
+const PageUp = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = () => {
+    window.scrollY > 850 ? setIsVisible(true) : setIsVisible(false);
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div
+      className={`page-up ${isVisible ? "visible" : ""}`}
+      onClick={scrollToTop}
+    >
+      <ion-icon name="arrow-up-circle-outline"></ion-icon>
+    </div>
+  );
+};
+
+export default PageUp;
