3bbbf98bbd10c3247cc7933a99810e32c851430d
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-11-10 22:06:48 -0300

    feat: creating provisional routing system

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "../src/styles/index.css";
+import Home from "./pages/Home.jsx";
+import Details from "./pages/Details.jsx";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "",
+    element: <App />,
+    children: [
+      // Componente base
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "details/:id",
+        element: <Details />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
