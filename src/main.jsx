cc483b12a0958436f9ea4d42f45e5c024094b595
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-11-11 19:27:55 -0300

    feat: adding dynamic route to specific country

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "../src/styles/index.css";
 import Home from "./pages/Home.jsx";
 import Details from "./pages/Details.jsx";
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
   {
-    path: "",
-    element: <App />,
-    children: [
-      // Componente base
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "details/:id",
-        element: <Details />,
-      },
-    ],
+    path: "/details/:countryName",
+    element: <Details />,
   },
 ]);
 
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,37 +1,52 @@
+import Header from "../components/Header";
+import data from "../../db/data.json";
 import { useParams } from "react-router-dom";
+import useDarkMode from "../hooks/useDarkMode";
+import "../styles/Details.css";
 
-const Details = ({ countries }) => {
+const Details = () => {
   const { countryName } = useParams();
+  const { isDarkMode, toggleDarkMode } = useDarkMode();
 
-  const country = countries.find((c) => c.name === countryName);
+  const countryDetails = data.find(
+    (country) => country.name.toLowerCase() === countryName.toLowerCase()
+  );
 
-  if (!country) {
-    return <div>Country not found</div>;
+  if (!countryDetails) {
+    return <div>País não encontrado</div>;
   }
 
   return (
-    <div>
+    <div
+      className={`container ${isDarkMode ? "dark-mode" : "light-container"}`}
+    >
+      <Header toggleDarkMode={toggleDarkMode} isDarkMode={isDarkMode} />
       <div className="flag-box">
-        <img src={country.flags.png} alt={`${country.name} Flag`} />
+        <img
+          src={countryDetails.flags.png}
+          alt={`${countryDetails.name} Flag`}
+        />
       </div>
       <div className="details-box">
-        <div className="country-name">{country.name}</div>
+        <div className="country-name">{countryDetails.name}</div>
       </div>
       <div className="details-container">
         <div className="geo-information">
-          <p>Native Name: {country.native}</p>
+          <p>Native Name: {countryDetails.nativeName}</p>
           <p>
             Population:{" "}
-            {country.population ? country.population.toLocaleString() : "N/A"}
+            {countryDetails.population
+              ? countryDetails.population.toLocaleString()
+              : "N/A"}
           </p>
-          <p>Region: {country.region}</p>
-          <p>Sub Region: {country.sub}</p>
-          <p>Capital: {country.capital}</p>
+          <p>Region: {countryDetails.region}</p>
+          <p>Sub Region: {countryDetails.subregion}</p>
+          <p>Capital: {countryDetails.capital}</p>
         </div>
         <div className="demograph-information">
-          <p>Top Level Domain: {country.level}</p>
-          <p>Currencies: {country.currency}</p>
-          <p>Languages: {country.lang}</p>
+          <p>Top Level Domain: {countryDetails.topLevelDomain}</p>
+          <p>Currencies: {countryDetails.currencies[0].name}</p>
+          <p>Languages: {countryDetails.languages[0].name}</p>
         </div>
       </div>
     </div>
