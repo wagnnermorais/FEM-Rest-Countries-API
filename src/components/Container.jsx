b2a9b8fb55961e7ecb514a3ed37aac5461efb18e
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-11-11 19:30:29 -0300

    style: removing hyperlink default style

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,25 +1,28 @@
 import "../styles/Container.css";
+import { Link } from "react-router-dom";
 
 const Container = ({ flag, alt, title, population, region, capital }) => {
   const formattedPopulation = population.toLocaleString();
   return (
-    <div className="country-box">
-      <div className="flag-box">
-        <img src={flag} alt={alt} width={250} height={200} />
+    <Link to={`details/${title}`} className="no-style-link">
+      <div className="country-box">
+        <div className="flag-box">
+          <img src={flag} alt={alt} width={250} height={200} />
+        </div>
+        <div className="country-data">
+          <h3 className="country-data-title">{title}</h3>
+          <p className="country-data-p">
+            Population: <span className="data">{formattedPopulation}</span>
+          </p>
+          <p className="country-data-p">
+            Region: <span className="data">{region}</span>
+          </p>
+          <p className="country-data-p">
+            Capital: <span className="data">{capital}</span>
+          </p>
+        </div>
       </div>
-      <div className="country-data">
-        <h3 className="country-data-title">{title}</h3>
-        <p className="country-data-p">
-          Population: <span className="data">{formattedPopulation}</span>
-        </p>
-        <p className="country-data-p">
-          Region: <span className="data">{region}</span>
-        </p>
-        <p className="country-data-p">
-          Capital: <span className="data">{capital}</span>
-        </p>
-      </div>
-    </div>
+    </Link>
   );
 };
 
