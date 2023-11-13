6a74295de0b5534e9a6fd450d973127f90c4ee20
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-11-12 23:53:38 -0300

    style: flexing the details container

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -18,35 +18,35 @@ const Details = () => {
 
   return (
     <div
-      className={`container ${isDarkMode ? "dark-mode" : "light-container"}`}
+      className={`details-box ${isDarkMode ? "dark-mode" : "light-container"}`}
     >
       <Header toggleDarkMode={toggleDarkMode} isDarkMode={isDarkMode} />
-      <div className="flag-box">
-        <img
-          src={countryDetails.flags.png}
-          alt={`${countryDetails.name} Flag`}
-        />
-      </div>
-      <div className="details-box">
-        <div className="country-name">{countryDetails.name}</div>
-      </div>
-      <div className="details-container">
-        <div className="geo-information">
-          <p>Native Name: {countryDetails.nativeName}</p>
-          <p>
-            Population:{" "}
-            {countryDetails.population
-              ? countryDetails.population.toLocaleString()
-              : "N/A"}
-          </p>
-          <p>Region: {countryDetails.region}</p>
-          <p>Sub Region: {countryDetails.subregion}</p>
-          <p>Capital: {countryDetails.capital}</p>
+      <div className="home-wrapper details">
+        <div className="flag-box">
+          <img
+            src={countryDetails.flags.png}
+            alt={`${countryDetails.name} Flag`}
+          />
         </div>
-        <div className="demograph-information">
-          <p>Top Level Domain: {countryDetails.topLevelDomain}</p>
-          <p>Currencies: {countryDetails.currencies[0].name}</p>
-          <p>Languages: {countryDetails.languages[0].name}</p>
+        <div className="details-container">
+          <div className="geo-information">
+            <div className="country-name">{countryDetails.name}</div>
+            <p>Native Name: {countryDetails.nativeName}</p>
+            <p>
+              Population:{" "}
+              {countryDetails.population
+                ? countryDetails.population.toLocaleString()
+                : "N/A"}
+            </p>
+            <p>Region: {countryDetails.region}</p>
+            <p>Sub Region: {countryDetails.subregion}</p>
+            <p>Capital: {countryDetails.capital}</p>
+          </div>
+          <div className="demograph-information">
+            <p>Top Level Domain: {countryDetails.topLevelDomain}</p>
+            <p>Currencies: {countryDetails.currencies[0].name}</p>
+            <p>Languages: {countryDetails.languages[0].name}</p>
+          </div>
         </div>
       </div>
     </div>
